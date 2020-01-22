548d163f1bdf080bd83f19e3acd46f2a9901f0d2
Dacod <clerk.dacod@gmail.com>
2020-01-22 15:30:39 +0200

    Updated to add relative internal links that start with / or ..

--- auto_target_blank.js
+++ auto_target_blank.js
@@ -5,11 +5,7 @@ window.onload = function(){
 
     console.log(window.location.hostname);
 
-  if(anchors[i].href.indexOf(window.location.hostname) >= 0){
-
-   anchors[i].setAttribute('target', '_self');
-
-  }else{
+  if(!((anchors[i].href.indexOf(window.location.hostname) >= 0) || anchors[i].href=="#" || anchors[i].href=="javascript:void(0)" || anchors[i].href[0]=="/" || anchors[i].href[0]==".")){
 
    anchors[i].setAttribute('target', '_blank');
 
